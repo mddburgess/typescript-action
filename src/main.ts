be5e05543cf7e2ac40cb54a811919fd3fec2c780
Mike Burgess <mddburgess@users.noreply.github.com>
2020-12-12 12:19:26 -0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+    const token = core.getInput('github-token')
+    const octokit = github.getOctokit(token)
+
+    core.debug('Creating check run')
+    const check = await octokit.checks.create({
+      ...github.context.repo,
+      name: 'sandbox',
+      head_sha: github.context.sha,
+      status: 'in_progress'
+    })
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
@@ -11,6 +23,14 @@ async function run(): Promise<void> {
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
+
+    core.debug('Updating check run')
+    await octokit.checks.update({
+      ...github.context.repo,
+      check_run_id: check.data.id,
+      status: 'completed',
+      conclusion: 'success'
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
